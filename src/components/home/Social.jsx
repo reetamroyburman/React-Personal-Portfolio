d06d9d7b8689939d7ebeb82e7b159c8773763425
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-06 21:34:15 +0530

    Add socialmedia component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/reetam-r-261782135/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/reetamroyburman" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+
+
+        <a href="https://www.behance.net/" className="home__social-icon" target="_blank">
+        <i class="uil uil-behance"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
