8efb74e27203102b24b7e07e9ce686f102312e62
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-08 10:08:40 +0530

    Add about component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import About from './components/about/About.jsx';
+import Home from './components/home/Home.jsx';
 import NavBar from './components/navbar/NavBar.jsx'
 function App() {
   return (
     <div>
       <NavBar/>
+
+      <main className='main'>
+        <Home/>
+        <About/>
+      </main>
     </div>
   );
 }
