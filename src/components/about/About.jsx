91c6f80a6340384d4e8ab90fbf53a0fe94891b48
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-08 10:24:54 +0530

    update about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './About.css'
+import AboutImg from "../../assets/about.jpg"
+import CV from "../../assets/Reetam-Cv.pdf"
+import Info from './Info'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about section" id="about">
+      <h2 className="section__title">about Me</h2>
+      <span className="section__subtitle">Hey there im a fullstack dev</span>
+
+      <div className="about__container container grid">
+        <img src={AboutImg} alt="" className="about__img" />
+          <div className="about__data">
+            <Info/>
+
+            <p className="about__description">..............</p>
+
+            <a href={CV} className="button button--flex"></a>
+          </div>
+      </div>
+    </section>
   )
 }
 
