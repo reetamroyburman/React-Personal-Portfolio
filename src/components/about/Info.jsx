97a9be039ecd9c92994492239d0082aad79f332e
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-09 14:00:57 +0530

    update about and info section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info">
+      <div className="about__box">
+      <i class='bx bx-award about__icon'></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">1year of experience</span>
+      </div>
+
+      <div className="about__box">
+      <i class='bx bx-briefcase-alt about__icon'></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">5-project</span>
+      </div>
+
+      <div className="about__box">
+      <i class='bx bx-support about__icon'></i>
+        <h3 className="about__title">support</h3>
+        <span className="about__subtitle">24/7</span>
+      </div>
+    </div>
   )
 }
 
