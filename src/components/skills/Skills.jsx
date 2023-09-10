035c2428a295ae780dec26e00f87647721499730
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-10 14:01:09 +0530

    create skill component

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+        <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My techinal skills</span>
+
+      <div className="skills__container container gird"></div>
+    </section>
+  )
+}
+
+export default Skills
