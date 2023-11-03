6c68b18cdfe3abe38a8451661c140a90973fc865
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-11-03 23:46:38 +0530

    service component changes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './services.css'
 
 const Services = () => {
   return (
@@ -9,8 +10,98 @@ const Services = () => {
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
-                    <i className="uil uil services__icon"></i>
-                    <h3 className="services__title"></h3>
+                    <i className="uil uil-web-grid services__icon"></i>
+                    <h3 className="services__title"> Product <br/> Designer</h3>
+                </div>
+
+                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">Product</h3>
+                        <p className="services__modal-description">something somthing decrpition</p>
+
+                        <ul className="services__modal-services-grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I am a fullstack Developer</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> Web page development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title"> Frontend <br/> Developer</h3>
+                </div>
+
+                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">Frontend</h3>
+                        <p className="services__modal-description">something somthing decrpition</p>
+
+                        <ul className="services__modal-services-grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I am a fullstack Developer</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> Web page development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-edit services__icon"></i>
+                    <h3 className="services__title">Backend <br/> Development</h3>
                 </div>
 
                 <span className="services__button">View More<i className="uil uil-arrow-right services__button-icon"></i></span>
@@ -18,13 +109,33 @@ const Services = () => {
                 <div className="services__modal">
                     <div className="services__modal-content">
                         <i className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__model-title"></h3>
-                        <p className="services__modal-description">something somthing</p>
+                        <h3 className="services__modal-title">Backend Development</h3>
+                        <p className="services__modal-description">something somthing decrpition</p>
 
                         <ul className="services__modal-services-grid">
                             <li className="services__modal-service">
                                 <i className="uil uil-check-circle services__modal-icon"></i>
                                 <p className="services__modal-info"> I am a fullstack Developer</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> Web page development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info"> I can build backend Server</p>
                             </li>
                         </ul>
                     </div>
